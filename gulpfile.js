06e50a261bef457c3a6c95dae225079cf4a835ba
Jovanica.K <jovanica0312@gmail.com>
2023-07-01 12:10:03 +0200

    Added Iconfont task and all required files

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const gulp = require ('gulp');
+
 const {
     src,
     dest,
@@ -15,19 +15,11 @@ const sass = require('gulp-sass')(require('sass'));
 const autoprefixer = require('gulp-autoprefixer');
 const cssnano = require('gulp-cssnano');
 const concat = require('gulp-concat');
-const clean = require('gulp-clean');
 const changed = require('gulp-changed');
 const browsersync = require('browser-sync').create();
 const sassLint = require('gulp-sass-lint');
-
-//  Clean assets
-
-function clear() {
-    return src('./assets/*', {
-            read: false
-        })
-        .pipe(clean());
-}
+const consolidate = require('gulp-consolidate');
+const iconfont = require('gulp-iconfont');
 
 // JS function 
 
@@ -94,6 +86,30 @@ function lint() {
         .pipe(browsersync.stream());
 }
 
+// Iconfont
+function icons() {
+    return src('src/images/svg/*.svg')
+    .pipe(iconfont({
+        fontName: 'iconfont',
+        formats: ['woff', 'woff2'],
+        appendCodepoints: true,
+        appendUnicode: false,
+        normalize: true,
+        fontHeight: 1000,
+        centerHorizontally: true
+    }))
+    .on('glyphs', function (glyphs, options) {
+        gulp.src('src/scss/config/iconfont-template/_iconfont.scss')
+            .pipe(consolidate('underscore', {
+                glyphs: glyphs,
+                fontName: options.fontName,
+                fontDate: new Date().getTime()
+            }))
+            .pipe(gulp.dest('src/scss/config'));
+    })
+    .pipe(dest('assets/fonts'));
+}
+
 // Watch files
 
 function watchFiles() {
@@ -117,4 +133,4 @@ function browserSync() {
 // Tasks to define the execution of the functions simultaneously or in series
 
 exports.watch = parallel(watchFiles, browserSync);
-exports.default = series(clear, parallel(html, js, css, img));
+exports.default = parallel(icons, html, js, css, img);
